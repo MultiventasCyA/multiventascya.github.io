83899c166e8ca3b0fefb0d049c8a9666691e4c0c
Alessandro <axl6513@gmail.com>
2025-07-10 14:43:16 -0600

    Update

--- main.js
+++ main.js
@@ -15,15 +15,22 @@ if ("loading" in HTMLImageElement.prototype) {
   console.log("`lazy-loading` no soportado...");
 }
 
-// link directo a WhatsApp
-document.querySelectorAll(".whatsapp-link").forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    var card = link.closest(".card");
-    var nombre = card.querySelector(".title-prod-1").innerText.trim();
-    var url =
-      "https://wa.me/529995823756?text=" +
-      encodeURIComponent("Hola, me interesa el producto: " + nombre);
-    window.open(url, "_blank");
-  });
-});
+// // link directo a WhatsApp
+// document.querySelectorAll(".whatsapp-link").forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     var card = link.closest(".card");
+//     var nombre = card.querySelector(".title-prod-1").innerText.trim();
+//     var url =
+//       "https://wa.me/529995823756?text=" +
+//       encodeURIComponent("Hola, me interesa el producto: " + nombre);
+//     window.open(url, "_blank");
+//   });
+// });
+
+function pedirProducto(btn) {
+  var card = btn.closest('.card');
+  var nombre = card.querySelector('.title-prod-1').innerText.trim();
+  var url = 'https://wa.me/529995823756?text=' + encodeURIComponent('Hola, me interesa el producto: ' + nombre);
+  window.open(url, '_blank');
+}
